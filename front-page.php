92d17c11c62318f05e5d388be1c954430e5b08eb
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-05-18 14:46:40 +0200

    Replace <img> by <p> in oscar section

--- front-page.php
+++ front-page.php
@@ -72,7 +72,7 @@ get_header();
         </section>
         <section id="oscar"  class="oscar">
             <article>
-                <img class="text-bg" src="<?php echo get_stylesheet_directory_uri () . './assets/images/oscar-texte.png'?>" alt="orange text background">
+                <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
                 <img class="oscar-img" src="<?php echo get_stylesheet_directory_uri () . './assets/images/oscar_nomination.png'?>" alt="orange text background">
             </article>
         </section>
