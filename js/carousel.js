8e03ad177c60d5fccd8f3411ea3835c50b986ca0
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-05-18 14:47:11 +0200

    add swiper carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,39 +1,7 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-
-//     const swiper = new Swiper(".swiper-container", {
-//         // Optional parameters
-//         // direction: "horizontal",
-
-//         // // Responsive breakpoints
-//         // breakpoints: {
-//         //     // when window width is >= 320px
-//         //     320: {
-//         //         slidesPerView: 2,
-//         //         spaceBetween: 20
-//         //     },
-//         //     // when window width is >= 480px
-//         //     480: {
-//         //         slidesPerView: 2,
-//         //         spaceBetween: 30
-//         //     },
-//         //     // when window width is >= 640px
-//         //     640: {
-//         //         slidesPerView: 3,
-//         //         spaceBetween: 10
-//         //     }
-//         // },
-
-//         // Enable touch drag
-//         touch: {
-//             enabled: true,
-//         },
-//     });
-
-// });
-
 const swiper = new Swiper(".swiper-container", {
     effect: "coverflow",
     grabCursor: true,
+    // loop: true,
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
