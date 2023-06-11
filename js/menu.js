4ee5d34d30cd56845442ed504ca0891c8b74683c
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-06-11 23:27:51 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,13 @@ const burger = document.getElementById('burger-btn');
 const fullscreenMenu = document.getElementById('menu-fullscreen');
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
+    if (burger.classList.contains('active')) {
+        burger.classList.remove('active');
+        burger.classList.add('inactive');
+    } else {
+        burger.classList.remove('inactive');
+        burger.classList.add('active');
+    }
     fullscreenMenu.classList.toggle('active');
 });
 
@@ -11,7 +17,7 @@ const menuLinks = document.querySelectorAll("#menu-fullscreen ul li a");
 menuLinks.forEach((link) => {
     link.addEventListener('click', () => {
         burger.classList.remove('active');
+        burger.classList.add('inactive');
         fullscreenMenu.classList.remove('active');
-
     });
 });
