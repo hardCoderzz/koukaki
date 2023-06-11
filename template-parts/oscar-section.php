3fe160b3ef742dc93682db7c16aa30068edf28a6
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-06-11 23:26:20 +0200

    Create template part for Oscar section

---
+++ template-parts/oscar-section.php
@@ -0,0 +1,6 @@
+<section id="oscar" class="oscar">
+    <article>
+        <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+        <img class="oscar-img" src="<?php echo get_stylesheet_directory_uri () . './assets/images/oscar_nomination.png'?>" alt="orange text background">
+    </article>
+</section>
