42ea3b51834e17e3f3e580e533ffdbc0556188a5
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-06-11 23:27:23 +0200

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,28 @@
-var swiper = new Swiper(".swiper-container", {
-    effect: "coverflow",
+const swiper = new Swiper('.swiper', {
+    effect: 'coverflow',
     grabCursor: true,
+    spaceBetween: 30,
+    slidesPerView: 'auto',
     centeredSlides: true,
-    slidesPerView: "auto",
-    spaceBetween: 40,
-    watchOverflow: true,
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: false,
+    //     coverflowEffect: {
+    //         rotate: 0,
+    //         stretch: 0,
+    //         depth: 100,
+    //         modifier: 1,
+    //         slideShadows: false,
+    //     }
+})
 
-    },
+// swiper.on('touchStart', function () {
+//     document.body.style.overflow = 'hidden';
+// });
 
+// swiper.on('touchEnd', function () {
+//     document.body.style.overflow = '';
+// });
 
-});
+// const swiperContainer = document.querySelector('.swiper-container');
 
-swiper.on('touchStart', function () {
-    document.body.style.overflow = 'hidden';
-});
-
-swiper.on('touchEnd', function () {
-    document.body.style.overflow = '';
-});
-
-const swiperContainer = document.querySelector('.swiper-container');
-
-swiperContainer.addEventListener('touchmove', function (e) {
-    e.preventDefault();
-}, { passive: false });
+// swiperContainer.addEventListener('touchmove', function (e) {
+//     e.preventDefault();
+// }, { passive: false });
